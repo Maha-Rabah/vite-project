af5eae374ea563d9546367f95da028718027eae3
Maha-Rabah <maharabah13@gmail.com>
2023-08-24 11:36:26 +0300

    add project

---
+++ src/data/user.js
@@ -0,0 +1,66 @@
+const INFO = {
+	main: {
+		title: "Maha Rabah",
+		name: "Maha R.",
+		email: "maharabah13@gmail.com",
+		logo: "../logo.png",
+	},
+
+	socials: {
+		twitter: "",
+		github: "https://github.com/Maha-Rabah",
+		linkedin: "https://linkedin.com/",
+		instagram: "https://instagram.com/",
+		stackoverflow: "https://stackoverflow.com/",
+		facebook: "https://facebook.com/",
+	},
+
+	homepage: {
+		title: "Front-End Developer.",
+		description:
+			"I am a Frontend developer with expertise in React.js. I have experience in building responsive websites that adapt to various screen sizes, ensuring an optimal user experience on both desktop and mobile platforms. I am proficient in using popular front-end frameworks such as Bootstrap and Foundation, which enable me to streamline the development process and create consistent designs.In addition to my technical skills, I also have a keen eye for design and aesthetics.",			
+	},
+
+	about: {
+		title: "I’m Maha R. I live in Damascuse, where I design the future.",
+		description:
+			"I've worked on a variety of projects over the years and I'm proud of the progress I've made. Many of these projects are open-source and available for others to explore and contribute to. If you're interested in any of the projects I've worked on, please feel free to check out the code and suggest any improvements or enhancements you might have in mind. Collaborating with others is a great way to learn and grow, and I'm always open to new ideas and feedback.",
+	},
+
+	articles: {
+		title: "I'm passionate about pushing the boundaries of what's possible and inspiring the next generation of innovators.",
+		description:
+			"Chronological collection of my long-form thoughts on programming, leadership, product design, and more.",
+	},
+
+	projects: [
+		{
+			title: "Project 1",
+			description:
+				"Lorem ipsum dolor sit amet. Et incidunt voluptatem ex tempore repellendus qui dicta nemo sit deleniti minima.",
+			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/javascript/javascript.png",
+			linkText: "View Project",
+			link: "https://github.com",
+		},
+
+		{
+			title: "Project 2",
+			description:
+				"Lorem ipsum dolor sit amet. Et incidunt voluptatem ex tempore repellendus qui dicta nemo sit deleniti minima.",
+			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/python/python.png",
+			linkText: "View Project",
+			link: "https://github.com",
+		},
+
+		{
+			title: "Project 3",
+			description:
+				"Lorem ipsum dolor sit amet. Et incidunt voluptatem ex tempore repellendus qui dicta nemo sit deleniti minima.",
+			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/html/html.png",
+			linkText: "View Project",
+			link: "https://github.com",
+		}
+	],
+};
+
+export default INFO;
